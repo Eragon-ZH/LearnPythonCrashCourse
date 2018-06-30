89fe592129e78806713bffc87e93cdc3b4d42649
Eragon-ZH <980019347@qq.com>
2018-06-30 21:35:21 +0800

    aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,9 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
+from game_stats import GameStats
 
 def run_game():
     # 初始化，创建屏幕对象
@@ -14,16 +16,23 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
+    # 创建一个用于统计游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
+    # 创建一艘飞船, 子弹编组， 外星人编组
     ship = Ship(screen, ai_settings)
-    # 创建一个存储子弹的编组
     bullets = Group()
+    aliens = Group()
+    # 创建外星人群
+    gf.creat_fleet(ai_settings, screen, ship, aliens)
 
     # 开始循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -9,9 +9,15 @@ class Settings():
         self.bg_color = (230, 230 ,230)
         # 飞船设置
         self.ship_speed_factor = 1.5
+        self.ships_limit = 3
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 20
+        # fleet_direction为1表示右移，-1表示左移
+        self.fleet_direction = 1
