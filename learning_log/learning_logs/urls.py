46bf9a2e369271a20084175cf4707c3f4336bc74
Eragon-ZH <980019347@qq.com>
2018-07-15 20:19:25 +0800

    use path

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,27 +1,28 @@
 """定义learning_logs的URL模式"""
 
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # 主页
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
 
-    # 显示所有的主题
-    url(r'^topics/$', views.topics, name='topics'),
+    # 显示所有主题
+    path('topics/', views.topics, name='topics'),
 
-    # 显示特点的主题
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # 单个主题
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 
-    # 用于添加新主题的网页
-    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # 添加新主题
+    path('new_topic/', views.new_topic, name='new_topic'),
 
-    # 用于添加新条目的页面
-    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # 添加新项目
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 
-    # 用于编辑条目的页面
-    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
+    # 编辑项目
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
 # 书上没有这一行在Django 2.0会报错
 app_name = "learning_logs"
