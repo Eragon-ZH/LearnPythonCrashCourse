30d5970c205f403b2fa54d24e1f6c77ac2c1c0ad
Eragon-ZH <980019347@qq.com>
2018-07-16 19:29:55 +0800

    use path

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,16 +1,20 @@
 """为应用程序users定义URL模式"""
 
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.views import login
 
 from . import views
 
 urlpatterns = [
-    # 登录页面
-    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
-    # 注册
-    url(r'^register/$', views.register, name='register'),
+    # 登录界面
+    path('login/', login, {'template_name': 'users/login.html'},
+        name='login'),
+
     # 注销
-    url(r'^logout/$', views.logout_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # 注册
+    path('register/', views.register, name='register'),
 ]
+
 app_name = 'users'
